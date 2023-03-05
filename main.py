15c5317255704fbc7ba3e0f7f87500d22db5077e
Badmajor <vtk88@mail.ru>
2023-03-05 22:40:24 +0300

    webhook

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher, Router
 
-from config import BOT_TOKEN
+from config import BOT_TOKEN, WEBHOOK_HOST, WEBHOOK_PATH
 from handlers import register_default_cmds
 
 logging.basicConfig(level=logging.INFO)
@@ -18,7 +18,11 @@ async def main():
     dp = Dispatcher()
     dp.include_router(router)
 
-    await dp.start_polling(bot)
+    await bot.set_webhook(
+        url=WEBHOOK_HOST + WEBHOOK_PATH,
+        drop_pending_updates=True,
+        allowed_updates=dp.resolve_used_update_types()
+    )
 
 if __name__ == "__main__":
     asyncio.run(main())
