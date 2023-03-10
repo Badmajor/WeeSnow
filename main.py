946c4d37f4ab82beb0ba495bd14b598786fe7137
Badmajor <vtk88@mail.ru>
2023-03-11 01:49:12 +0300

    1

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher, Router
+from aiogram.webhook.aiohttp_server import SimpleRequestHandler
+from aiohttp import web
 
-
-from config import BOT_TOKEN, WEBHOOK_HOST, WEBHOOK_PATH
+from config import BOT_TOKEN, WEBHOOK_HOST, WEBHOOK_PATH, WEBAPP_HOST, WEBAPP_PORT
 from handlers import register_routers
 
 logging.basicConfig(level=logging.INFO)
@@ -24,6 +25,15 @@ async def main():
         drop_pending_updates=True,
         allowed_updates=dp.resolve_used_update_types()
     )
+    app = web.Application()
+    SimpleRequestHandler(dispatcher=dp, bot=bot).register(app, path=WEBHOOK_PATH)
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, host=WEBAPP_HOST, port=WEBAPP_PORT)
+    await site.start()
+
+    # Running it forever
+    await asyncio.Event().wait()
 
 if __name__ == "__main__":
     asyncio.run(main())
