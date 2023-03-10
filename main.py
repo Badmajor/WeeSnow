cc3e05f65edefced778f098e8dcbb05c33daf918
Badmajor <vtk88@mail.ru>
2023-03-11 00:00:34 +0300

    loop

--- main.py
+++ main.py
@@ -24,9 +24,8 @@ async def main():
         drop_pending_updates=True,
         allowed_updates=dp.resolve_used_update_types()
     )
+    await bot.close()
 
 if __name__ == "__main__":
-    # asyncio.run(main())
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
-    loop.close()
+    asyncio.run(main())
+
