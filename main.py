d2f7b0a26b1f159c4a97980adc75389bcd7114b8
Badmajor <vtk88@mail.ru>
2023-03-10 23:51:44 +0300

    python-3.9

--- main.py
+++ main.py
@@ -26,7 +26,4 @@ async def main():
     )
 
 if __name__ == "__main__":
-    # asyncio.run(main())
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
-    loop.close()
+    asyncio.run(main())
